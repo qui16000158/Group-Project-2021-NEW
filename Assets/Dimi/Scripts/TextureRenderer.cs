f3ff0eba329ccdfa054246d70e4898a1610f8ac1
Silenthowler <23553428+domcrouse@users.noreply.github.com>
2021-05-03 14:19:56 +0100

    Hazard testing

---
+++ Assets/Dimi/Scripts/TextureRenderer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureRenderer : MonoBehaviour
+{
+    void Start()
+    {
+        Texture2D texture = new Texture2D(128, 128);
+        Renderer renderer = GetComponent<Renderer>();
+        renderer.material.mainTexture = texture;
+    }
+}
