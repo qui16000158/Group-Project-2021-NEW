81df7154dc1dba8f381d386569eef20cda376d46
qui16000158 <55698561+qui16000158@users.noreply.github.com>
2021-05-13 07:44:43 +0100

    Added map, water, basic snowball script

---
+++ Assets/Scripts/Snowball.cs
@@ -0,0 +1,27 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snowball : NetworkBehaviour
+{
+    [SerializeField]
+    GameObject iceBlockPrefab;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (isServer)
+        {
+            if (other.tag == "Water")
+            {
+                Vector3 pos = transform.position;
+
+                Destroy(gameObject);
+
+                GameObject iceBlock = Instantiate(iceBlockPrefab, pos, Quaternion.identity);
+
+                NetworkServer.Spawn(iceBlock);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/SnowballThrow.cs
@@ -0,0 +1,39 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowballThrow : NetworkBehaviour
+{
+    [SerializeField]
+    GameObject snowBallPrefab;
+    [SerializeField]
+    Transform hand; // The player's hand position
+    [SerializeField]
+    float throwStrength = 10.0f;
+
+    new Transform camera;
+
+    private void Start()
+    {
+        camera = Camera.main.transform;
+    }
+
+    [Command]
+    void CmdThrow(Vector3 spawnPos, Vector3 normal)
+    {
+        GameObject snowBall = Instantiate(snowBallPrefab, spawnPos, Quaternion.identity);
+        NetworkServer.Spawn(snowBall);
+        snowBall.GetComponent<Rigidbody>().velocity = normal * throwStrength;
+    }
+
+    private void Update()
+    {
+        if (!isLocalPlayer) return;
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            CmdThrow(hand.position, camera.forward);
+        }
+    }
+}
