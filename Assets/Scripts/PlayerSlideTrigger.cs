1ae0a275ac8a4a223e7da035167887132a691b00
qui16000158 <55698561+qui16000158@users.noreply.github.com>
2021-05-19 13:48:36 +0100

    Slide Test

---
+++ Assets/Scripts/PlayerSlideTrigger.cs
@@ -0,0 +1,17 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSlideTrigger : NetworkBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!isLocalPlayer) return;
+
+        if(other.TryGetComponent(out SlideJump slide))
+        {
+            slide.StartSlide(transform);
+        }
+    }
+}
---
+++ Assets/Scripts/SlideJump.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlideJump : MonoBehaviour
+{
+    [SerializeField]
+    GameObject slidePrefab; // The prefab for the slide
+    [SerializeField]
+    Vector3 rotationOffset; // The offset for the player's rotation when sliding
+
+    public void StartSlide(Transform player)
+    {
+        Transform slide = Instantiate(slidePrefab, player.position, Quaternion.identity).transform;
+
+        Transform agent = slide.Find("Slide Anchor");
+
+        player.parent = agent;
+
+        Debug.Log(player.parent);
+
+        Animator anim = slide.GetComponent<Animator>();
+
+        anim.SetTrigger("Start Slide");
+    }
+}
